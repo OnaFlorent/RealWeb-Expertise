741f7a4fced368373108439cc9802d6907859c9c
OnaFlorent <bflorent53170@gmail.com>
2024-11-07 22:12:16 +0100

    Firebase/SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Tools } from "./components/Tools";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
@@ -10,11 +11,66 @@ import { FileDownIcon, FileUser } from "lucide-react";
 
 function App() {
   return (
-    <>
+    <HelmetProvider>
+      <Helmet>
+        <title>Florent Belot | Développeur Full-Stack Freelance sur la métropole Lilloise
+        - Création de Sites Web & Applications SaaS - TJM 250/j</title>
+        <meta
+          name="description"
+          content="Florent Belot | Développeur Full-Stack Freelance sur la métropole Lilloise
+      - Création de Sites Web & Landing Page & Applications Web & SaaS - TJM 250/j"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Florent Belot | Développeur Full-Stack Freelance sur la métropole Lilloise
+      - Création de Sites Web & Landing Page & Applications Web & SaaS - TJM 250/j"
+        />
+        <meta
+          property="og:description"
+          content="Florent Belot | Développeur Full-Stack Freelance sur la métropole Lilloise
+      - Création de Sites Web & Landing Page & Applications Web & SaaS - TJM 250/j"
+        />
+        <meta
+          property="og:image"
+          content="https://belotflorent.fr/src/assets/profile_img.png"
+        />
+      </Helmet>
+
       <Header />
+
+      {/* Section Hero avec balises SEO spécifiques */}
+      <Helmet>
+        <title>Accueil - Florent Belot, Développeur Full-Stack</title>
+        <meta
+          name="description"
+          content="Bienvenue sur le portfolio de Florent Belot, développeur Full-Stack freelance basé à Roubaix."
+        />
+      </Helmet>
       <Hero />
+
+      {/* Section Outils */}
+      <Helmet>
+        <title>Outils et Technologies - Florent Belot</title>
+        <meta
+          name="description"
+          content="Découvrez les outils et technologies utilisés par Florent Belot pour réaliser des projets de développement web modernes et efficaces."
+        />
+      </Helmet>
       <Tools />
+
+      {/* Section Portfolio */}
+      <Helmet>
+        <title>Portfolio - Florent Belot</title>
+        <meta
+          name="description"
+          content="Découvrez les projets réalisés par Florent Belot, développeur Full-Stack freelance, pour des clients de divers secteurs."
+        />
+      </Helmet>
       <Portfolio />
+
+      {/* Call to Action - CV */}
       <CallToAction
         minify={true}
         title="CV développeur Full-Stack"
@@ -30,20 +86,43 @@ function App() {
         buttonText="Télécharger"
         buttonLink="https://drive.google.com/file/d/18aF6lSHo-wOrV1PGWWgCEXnBcp7hnIUa/view"
       />
+
+      {/* Section Tarifs */}
+      <Helmet>
+        <title>Tarifs - Florent Belot, Développeur Full-Stack</title>
+        <meta
+          name="description"
+          content="Découvrez les tarifs pour les forfaits clé en main de développement d'applications web offerts par Florent Belot, développeur Full-Stack freelance."
+        />
+      </Helmet>
       <Pricing />
+
+      {/* Call to Action - Contact */}
       <CallToAction
         minify={false}
         title="Besoin d'une solution sur mesure ?"
-        description="Si vous avez besoin d'un site vitrine plus élaboré ou d'un site e-commerce, n'hésitez pas à me contacter pour une estimation via ce formulaire:"
+        description="Si vous avez besoin d'un site vitrine plus élaboré ou d'un site e-commerce, n'hésitez pas à me contacter pour une estimation via ce formulaire :"
         image={
           <FileUser width={24} height={24} alt="Google_form" className="mr-2" />
         }
         buttonText="Prendre contact"
         buttonLink="https://forms.gle/SQePZgWNcg5ucBB97"
       />
+
+      {/* Section Services */}
+      <Helmet>
+        <title>
+          Services - Florent Belot, Développeur Full-Stack à Roubaix
+        </title>
+        <meta
+          name="description"
+          content="Florent Belot propose des services de développement web sur mesure : création de sites vitrines, applications SaaS, landing page et plus encore."
+        />
+      </Helmet>
       <Services />
+
       <Footer />
-    </>
+    </HelmetProvider>
   );
 }
 
